74b714016b88f0d01f0d3848b0b03309f98fcfcc
taoyuzhu <573691723@qq.com>
2020-04-28 22:07:28 +0800

    zhiqiu_mac_20200427

--- meng-manage/src/main/java/com/manage/sys/controller/SysMenuController.java
+++ meng-manage/src/main/java/com/manage/sys/controller/SysMenuController.java
@@ -6,6 +6,7 @@ import com.core.entity.sys.Result;
 import com.core.entity.sys.SysMenu;
 import com.manage.sys.service.impl.SysMenuServiceImpl;
 import io.swagger.annotations.Api;
+import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,12 +27,12 @@ public class SysMenuController extends AbstractController {
 
 	@GetMapping("/menuRouter")
 	public Result queryMenuRouter() {
-		List<SysMenu> sysMenus = sysMenuService.queryMenuRouterByUserId(99);
+		List<SysMenu> sysMenus = sysMenuService.queryMenuRouterByUserId(0);
 		return Result.ok().put("sysMenus", sysMenus);
 	}
 
 	@GetMapping("/menuTree")
-//	@RequiresPermissions(value = { "sys:menu:list", "sys:menu:info" }, logical = Logical.OR)
+	@RequiresPermissions(value = { "sys:menu:list", "sys:menu:info" }, logical = Logical.OR)
 	@Log(value = "权限树")
 	public Result queryMenuTree() {
 		List<SysMenu> sysMenus = sysMenuService.querySysMenuTree(0);
